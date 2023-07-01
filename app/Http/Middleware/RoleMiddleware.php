20b21f03c1e07b658803a9f05aa79cd704ee30e3
uzielmvuama <uzielmvuama@gmail.com>
2023-07-01 19:46:52 -0400

    Update

--- app/Http/Middleware/RoleMiddleware.php
+++ app/Http/Middleware/RoleMiddleware.php
@@ -8,20 +8,32 @@ use Illuminate\Support\Facades\Auth;
 
 class RoleMiddleware
 {
-    public function handle(Request $request, Closure $next, $roles)
+    public function handle(Request $request, Closure $next)
     {
+
         $user = Auth::user();
-
-        if (!$user) {
-            return redirect()->route('login');
-        }
-
-        foreach ($roles as $role) {
-            if ($user->role === $role) {
+        $url = $request->path();
+        if (preg_match('/^admin/', $url)) {
+            if ($user->role == 'admin') {
                 return $next($request);
+            } else {
+                dd('Vous n\'avez pas de droit administrateur');
             }
+        } else {
+            return $next($request);
         }
 
-        return redirect()->back()->withErrors(['error' => 'This page is reserved for '.$roles.' only']);
+
+        // if (!$user) {
+        //     return redirect()->route('login');
+        // }
+
+        // foreach ($roles as $role) {
+        //     if ($user->role === $role) {
+        //         return $next($request);
+        //     }
+        // }
+
+        // return redirect()->back()->withErrors(['error' => 'This page is reserved for '.$roles.' only']);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -3,20 +3,38 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\HomeController;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
 
 
 Route::get('/', function () {
+    
+    // User::create(
+    //     [
+    //         'name' => 'admin',
+    //         'email' => 'admin@gmail.com',
+    //         'password' => '1234'
+    //     ]
+    // );
     return view('welcome');
 });
 
 Route::get('login', [AuthController::class, 'showLoginForm'])->name('login');
 Route::post('login', [AuthController::class, 'login']);
 Route::post('logout', [AuthController::class, 'logout'])->name('logout');
-Route::middleware(['auth', 'admin:roles'])->group(function () {
-    Route::get('home/admin', [HomeController::class, 'showAdminHome'])->name('home.admin');
-});
 
-Route::middleware(['auth', 'client:roles'])->group(function () {
+// MY CODE
+Route::middleware(['auth', 'roleControl'])->group(function () {
+    Route::get('/admin', [HomeController::class, 'showAdminHome'])->name('home.admin');
     Route::get('home/client', [HomeController::class, 'showClientHome'])->name('home.client');
 });
+
+
+// TON VIEUX CODE
+// Route::middleware(['auth', 'role:admin'])->group(function () {
+//     Route::get('home/admin', [HomeController::class, 'showAdminHome'])->name('home.admin');
+// });
+
+// Route::middleware(['auth', 'role:client'])->group(function () {
+//     Route::get('home/client', [HomeController::class, 'showClientHome'])->name('home.client');
+// });
